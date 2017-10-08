85808169f1504d1da6cf6cede2d4033986a81819
dxy19920817 <NULL>
2017-10-08 22:08:52 +0800

    123456

---
+++ test_dd.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+import os
+import socket
+import struct
+import binascii
+import dpkt
+import pcap
+import thread
+#from Tkinter import *
+import Tkinter
+
+
+
+class GUITest:
+    def __init__(self):
+        pass
+
+if __name__ == "__main__":
+    print "Hello world!"
+    #root  = Tk()
+    #root.mainloop()
+    #root.destroy()
+
